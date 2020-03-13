4263134b9d3784fa016516c6bd7eb56f0dbc623c
tfpoivic <tfpoivic@gmail.com>
2020-03-13 11:00:05 +0800

    add url

---
+++ app/src/main/java/tw/dp103g3/itfood/main/Url.java
@@ -0,0 +1,7 @@
+package tw.dp103g3.itfood.main;
+
+public class Url {
+    public static String URL = "http://172.20.10.2:8080/Itfood_Web/";
+    public static String ORDER_SOCKET_URI = "ws://172.20.10.2:8080/Itfood_Web/OrderSocket/mem";
+    public static String DELIVERY_SOCKET_URI = "ws://172.20.10.2:8080/Itfood_Web/DeliverySocket/mem";
+}
